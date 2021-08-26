ad4ba31c50456a6f6bffd20023a3c9bc3a732208
Lorenz Koestler <lorenz@koestler.ch>
2021-08-26 13:11:50 +0200

    cleanup, add default route

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import { Router } from 'preact-router';
+import { Router, route } from 'preact-router';
 
 import Header from './header';
 import { CubeGrid } from 'styled-loaders'
@@ -11,7 +11,6 @@ const App = () => {
 	const [config, setConfig] = useState(null)
 	useEffect(() => {
 		fetch(`/api/v0/config`)
-			.then(res => new Promise(resolve => setTimeout(() => resolve(res), 500)))
 			.then(res => res.json())
 			.then(data => setConfig(data))
 	}, []);
@@ -22,16 +21,30 @@ const App = () => {
 		</div>
 	}
 
-	console.log(config)
-
 	return (
 		<div id="app">
 			<Header title={config.projectTitle} views={config.views} />
 			<Router>
 				{ config.views.map(view => <View key={view.name} path={`/${view.name}`} {...view} />)}
+				<DefaultRoute default views={config.views} />
 			</Router>
 		</div>
 	)
+}
+
+const DefaultRoute = ({views}) => {
+	if (views.count < 1) {
+		return (
+	<div class="content">
+		<h1>Empty</h1>
+		<p>No views are defined. Check your config.yaml</p>
+	</div>
+);
+	}
+
+	const defaultView = views[0]
+	route(`/${defaultView.name}`, true)
+	return null
 }
 
 export default App;
--- src/components/view/index.js
+++ src/components/view/index.js
@@ -4,10 +4,10 @@ import Image from "../image";
 
 const View = ({title, name, cameras}) => {
 	return (
-		<div class={style.profile}>
+		<div class="content">
 			<h1>{title}</h1>
-			<ul>
-				{cameras.map(camera => <li>
+			<ul class={style.images}>
+				{cameras.map(camera => <li key={camera}>
 					<Image viewName={name} cameraName={camera} />
 				</li>)}
 			</ul>
