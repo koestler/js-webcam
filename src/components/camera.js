e277efe48581b56d8ebcdffb2e334113d458833c
Lorenz Koestler <lorenz@koestler.ch>
2021-09-20 10:38:06 +0200

    implement autoplay

--- src/components/camera.js
+++ src/components/camera.js
@@ -4,21 +4,32 @@ import { Image, Message } from 'react-bulma-components'
 import { AutoplayContext } from './autoplay'
 import HideableMessage from './hideableMessage'
 
+const fetchImage = (imgUrl, setImgSrc, autoplay) => {
+  window.fetch(imgUrl)
+    .then(response => {
+      // restart fetch image when autoplay enabled
+      if (autoplay.play) {
+        setTimeout(() => fetchImage(imgUrl, setImgSrc, autoplay), autoplay.refreshIntervalMs)
+      }
+
+      return response.blob()
+    })
+    .then(blob => new Promise(resolve => {
+      const reader = new window.FileReader()
+      reader.onload = function () { resolve(this.result) }
+      reader.readAsDataURL(blob)
+    }))
+    .then(setImgSrc)
+}
+
 const Camera = ({ viewName, cameraName, cameraTitle }) => {
   const autoplay = useContext(AutoplayContext)
   const [imgSrc, setImgSrc] = useState(null)
 
   useEffect(() => {
     const imgUrl = `/api/v0/images/${viewName}/${cameraName}.jpg`
-    window.fetch(imgUrl)
-      .then(response => response.blob())
-      .then(blob => new Promise(resolve => {
-        const reader = new window.FileReader()
-        reader.onload = function () { resolve(this.result) }
-        reader.readAsDataURL(blob)
-      }))
-      .then(setImgSrc)
-  }, [viewName, cameraName])
+    fetchImage(imgUrl, setImgSrc, autoplay)
+  }, [viewName, cameraName, autoplay.play])
 
   return (
     <HideableMessage header={<p>{cameraTitle}</p>}>
