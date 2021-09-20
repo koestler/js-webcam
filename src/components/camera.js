9b9b3d0ec837e8841347a26ce114b85d4f1af345
Lorenz Koestler <lorenz@koestler.ch>
2021-09-20 14:25:55 +0200

    implement correct starting / stoping of autoplay and component cleanup

--- src/components/camera.js
+++ src/components/camera.js
@@ -1,17 +1,18 @@
 import React, { useContext, useState, useEffect } from 'react'
-
 import { Image, Message } from 'react-bulma-components'
 import { AutoplayContext } from './autoplay'
 import HideableMessage from './hideableMessage'
 
-const fetchImage = (imgUrl, setImgSrc, autoplay) => {
+const fetchImage = (imgUrl, setImgSrc, play, refreshIntervalMs, setTimer) => {
+  const fetchStarted = Date.now()
   window.fetch(imgUrl)
     .then(response => {
       // restart fetch image when autoplay enabled
-      if (autoplay.play) {
-        setTimeout(() => fetchImage(imgUrl, setImgSrc, autoplay), autoplay.refreshIntervalMs)
+      if (play) {
+        const fetchTime = Date.now() - fetchStarted
+        const interval = Math.max(refreshIntervalMs - fetchTime, 0)
+        setTimer(setTimeout(() => fetchImage(imgUrl, setImgSrc, play, refreshIntervalMs, setTimer), interval))
       }
-
       return response.blob()
     })
     .then(blob => new Promise(resolve => {
@@ -23,19 +24,34 @@ const fetchImage = (imgUrl, setImgSrc, autoplay) => {
 }
 
 const Camera = ({ viewName, cameraName, cameraTitle }) => {
-  const autoplay = useContext(AutoplayContext)
+  const {play, refreshIntervalMs} = useContext(AutoplayContext)
+  const [initial, setInitial] = useState(true)
   const [imgSrc, setImgSrc] = useState(null)
+  const [timer, setTimer] = useState(null)
 
   useEffect(() => {
-    const imgUrl = `/api/v0/images/${viewName}/${cameraName}.jpg`
-    fetchImage(imgUrl, setImgSrc, autoplay)
-  }, [viewName, cameraName, autoplay.play])
+    if (initial || (play && timer === null)) {
+      fetchImage( `/api/v0/images/${viewName}/${cameraName}.jpg`, setImgSrc, play, refreshIntervalMs, setTimer)
+      setInitial(false)
+    }
+    return () => {
+      // cleanup on unmout
+      clearTimeout(timer)
+    }
+  }, [viewName, cameraName, initial, setInitial, setImgSrc, timer, setTimer, play, refreshIntervalMs])
+
+  useEffect(() => {
+    // stop refresh timer
+    if (!play) {
+      clearTimeout(timer)
+      setTimer(null)
+    }
+  }, [play, timer, setTimer])
 
   return (
     <HideableMessage header={<p>{cameraTitle}</p>}>
       <Message.Body>
         {imgSrc && <Image src={imgSrc} />}
-        {autoplay.play && <p>refresh every {autoplay.refreshIntervalMs}</p>}
       </Message.Body>
     </HideableMessage>
   )
